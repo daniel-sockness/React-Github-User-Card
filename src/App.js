cbd0b4aeab921971356bae39d0b69063519500ad
daniel-sockness <danielsock@gmail.com>
2019-11-03 12:33:18 -0600

    Generated react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+
+
+  render() {
+    console.log("App.js is rendering");
+    return(
+      <h1>Test</h1>
+    );
+  }
+}
+
+export default App;
